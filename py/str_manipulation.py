bcf48b6e4ed353097442b6935c3d9f79b2d40417
mohdadibfikrikamar <mohdadibfikrikamar@gmail.com>
2026-08-03 12:35:31 +0800

    first commit

---
+++ py/str_manipulation.py
@@ -0,0 +1,38 @@
+##String manipulation functions
+
+name = "Adib"
+age = 41
+
+message_1 = f"Hello, my name is {name} and I am {age} years old."
+
+print(message_1)
+
+## Exercise 1
+
+message = """HariNi"""
+message_1 = """  HariNi"""
+
+print(len(message))
+print(len(message_1))
+print(message_1.strip())
+print(message.upper())
+print(message.lower())
+print(message.title())
+print(message.replace("i", "e"))
+
+## Exercise 2
+## Build  a  simple  text  analyzer  that  counts  words,  characters,  andsentences in a given text.text
+
+message = f"""Python is a powerful programming language. It's easy to learn and versatile!You  can  use  Python  for  web  development,  data  science,  andautomation. The syntax is clean and readable.This makes Python perfect for beginners and experts alike."""
+
+message_1 = f"""Python is a powerful programming language. It's easy to learn \n""""""coding"""
+
+message_2 = f"""My age: {age}"""
+
+print("Number of words:", len(message.split())) ## count words
+print("Number of characters:", len(message)) ## characters (ie: alphabets, spaces, numbers)
+print("Number of sentences:", message.count(".") + message.count("!") + message.count("?")) 
+print("Number of sentences:", message.count("?")) ## count only sentence with question mark
+print(message_1)
+print("Number of sentences:", message_1.count("a"))
+print(message_2)
---
+++ py/var_data_types.py
@@ -0,0 +1,26 @@
+## variables and data types
+
+name = "adib"
+age = 41
+weight = 140.5
+status = True
+bank = None
+
+## Exercise 1
+
+print(name)
+print(age)
+print(type(weight))
+print(type(status))
+print(type(bank))
+
+## Exercise 2
+
+x = 25
+y = 30
+
+y = x * 9/5 + 32
+print(y)
+
+temp = f"The temperature is {y} degrees Fahrenheit."
+print(temp)
